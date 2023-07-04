aeabe3b8519d25325b78fac2aae4bd7a841924e4
Hyeyoung Yun <93719660+yhy1214@users.noreply.github.com>
2023-07-04 21:28:32 +0900

    Add image carousel
    - Add image carousel to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
+import Products from "./Products";
+import { getProducts } from "../database/firebase";
 
 export default function Home() {
-    return (
-        <div>
-            <p>Home</p>
-        </div>
-    );
-}
+  const { data: products } = useQuery(["products"], getProducts);
 
+  const [curr, setCurr] = useState(0);
+
+  const prev = () => {
+    setCurr((curr) => (curr === 0 ? products.length - 1 : curr - 1));
+  };
+  const next = () => {
+    setCurr((curr) => (curr === products.length - 1 ? 0 : curr + 1));
+  };
+
+  useEffect(() => {
+    const slideInterval = setInterval(next, 4000);
+    return () => clearInterval(slideInterval);
+  }, [products]);
+
+  return (
+    <div className="w-full h-1/2 bg-gray-100 relative">
+      <div className="absolute inset-0 p-40 flex items-center justify-between">
+        <ChevronLeftIcon
+          onClick={prev}
+          className="w-10 h-10 text-gray-300 cursor-pointer"
+        />
+        <ChevronRightIcon
+          onClick={next}
+          className="w-10 h-10 text-gray-300 cursor-pointer"
+        />
+      </div>
+      <div className="relative max-w-xs m-auto overflow-hidden rounded-lg">
+        <div
+          className="flex transition-transform ease-in-out duration-1000"
+          style={{ transform: `translateX(-${curr * 100}%)` }}
+        >
+          {products &&
+            products.map((product) => {
+              return (
+                <img
+                  src={product.imgSrc}
+                  alt={product.name}
+                  className="cover"
+                />
+              );
+            })}
+        </div>
+        <div className="absolute bottom-8 right-0 left-0">
+          <div className="flex items-center justify-center gap-2">
+            {products &&
+              products.map((product, index) => {
+                return (
+                  <div
+                    onClick={() => setCurr(index)}
+                    className={`transition-all w-3 h-3 bg-white rounded-full cursor-pointer ${
+                      curr === index ? "p-2" : "bg-opacity-50"
+                    }`}
+                  />
+                );
+              })}
+          </div>
+        </div>
+      </div>
+
+      <Products />
+    </div>
+  );
+}
