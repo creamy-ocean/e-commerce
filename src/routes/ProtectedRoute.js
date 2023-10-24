5044fb0b015094b756a5373e1f93f7db93ae3f22
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2023-10-24 14:46:23 +0900

    [Refactor] ProtectedRoute 컴포넌트 추가
    - 로그인 하지 않은 경우 장바구니 페이지에 접근 불가하도록 개선
    - 어드민 계정이 아닌 경우 어드민 페이지에 접근 불가하도록 개선

---
+++ src/routes/ProtectedRoute.js
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
+
+const ProtectedRoute = ({ children, adminRequired }) => {
+  const { user } = useAuthContext();
+
+  return !user || (adminRequired && !user.isAdmin) ? (
+    <Navigate to="/" replace />
+  ) : (
+    children
+  );
+};
+
+export default ProtectedRoute;
