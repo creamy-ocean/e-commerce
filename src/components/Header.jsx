3d35391565aca769f581d2695d3d06c6b0ccceb2
Hyeyoung Yun <hyeyoungyun1214@gmail.com>
2025-10-31 23:34:38 +0800

    [Refactor] Fixed profile image issue
    Fixed issue where Google account profile image was not showing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -71,6 +71,7 @@ export default function Example() {
                       className="h-8 w-8 rounded-full"
                       src={user.photoURL}
                       alt={user.displayName}
+                      referrerPolicy="no-referrer"
                     />
                     <ChevronDownIcon
                       className="-mr-1 h-5 w-5 text-gray-400"
